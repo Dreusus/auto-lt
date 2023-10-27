5db76360539af5cd848d650107e16d6810574243
dreusus <pitakxdd@gmail.com>
2023-10-27 19:02:10 +0300

    LoginPage,WebmastersPage fixed locator

--- src/test/java/pages/Navigate.java
+++ src/test/java/pages/Navigate.java
@@ -1,14 +1,13 @@
 package pages;
 
-import org.openqa.selenium.By;
 import static com.codeborne.selenide.Selenide.$;
 
 public class Navigate {
 
-    private static final By WEBMASTERS_NAVIGATION_LINK = By.xpath("//li[@class='dropdown']/a[text()='Вебмастера ']");
-    private static final By WEBMASTER_DROPDOWN_LINK = By.xpath("//ul/li/a[text()='Вебмастера']");
-    private static final By ADVERTISER_NAVIGATION_LINK = By.xpath("//li[@class='dropdown']/a[text()='Рекламодатели ']");
-    private static final By OFFERS_DROPDOWN_LINK = By.xpath("//ul/li/a[text()='Офферы']");
+    private static final String WEBMASTERS_NAVIGATION_LINK = "//li[@class='dropdown']/a[text()='Вебмастера ']";
+    private static final String WEBMASTER_DROPDOWN_LINK = "//ul/li/a[text()='Вебмастера']";
+    private static final String ADVERTISER_NAVIGATION_LINK = "//li[@class='dropdown']/a[text()='Рекламодатели ']";
+    private static final String OFFERS_DROPDOWN_LINK = "//ul/li/a[text()='Офферы']";
 
     public void navigateToWebmastersPage() {
         $(WEBMASTERS_NAVIGATION_LINK).click();
