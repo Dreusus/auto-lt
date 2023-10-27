2ef2282d4e895834f18906e0115056d2ae928cac
dreusus <pitakxdd@gmail.com>
2023-10-27 21:29:24 +0300

    create description for methods classes OfferCreationPage

--- src/test/java/pages/advertiser/OfferCreationPage.java
+++ src/test/java/pages/advertiser/OfferCreationPage.java
@@ -8,78 +8,48 @@ import java.io.File;
 import static com.codeborne.selenide.Selenide.*;
 
 public class OfferCreationPage {
-    public static final String pathImage = "image.png";
-    private Faker faker = new Faker();
-    private SelenideElement buttonSave = $x("//button[@type='submit' and text()='Сохранить']");
-    private SelenideElement imageFileButton = $("#offer-imagefile");
-    private SelenideElement favImageFileButton = $("#offer-favimagefile");
-    private SelenideElement name = $("#offer-name");
-    private SelenideElement slug = $("#offer-slug");
-    private SelenideElement licence = $("#offer-licensenumber");
-    private SelenideElement erid = $("#offer-erid");
-    private SelenideElement hold = $("#offer-holdseconds");
-    private SelenideElement minAge = $("#offer-age_min");
-    private SelenideElement maxAge = $("#offer-age_max");
-    private SelenideElement offerLink = $("#offer-link");
-    private SelenideElement statusDropdown = $x("//span[text()='Выберите статус...']");
-    private SelenideElement activeStatus = $x("//ul/li[text()='Активен']");
-    private SelenideElement privateStatus = $x("//ul/li[text()='Приватный']");
-    private SelenideElement noActiveStatus = $x("//ul/li[text()='Неактивен']");
-    private SelenideElement deleteStatus = $x("//ul/li[text()='Удален']");
-    private SelenideElement offerProductName = $("#offer-productname");
-    private SelenideElement advertiserDropdown = $x("//span[@id='select2-offer-advertiserid-container']");
-    private SelenideElement advertiserTest = $x("//ul/li[text()='181 TestAdvertis']");
-    private SelenideElement advertiserFirst = $x("//ul[@id='select2-offer-advertiserid-results']/li[1]");
+    // Константы и генератор случайных данных
+    public static final String PATH_IMAGE = "image.png";
+    private final Faker faker = new Faker();
 
-    private  SelenideElement currencyDropdown = $x("//span[@id='select2-offer-currency-container']");
-    private  SelenideElement currencyFirst = $x("//ul[@id='select2-offer-currency-results']/li[1]");
+    // Элементы управления и поля
+    private final SelenideElement buttonSave = $x("//button[@type='submit' and text()='Сохранить']");
+    private final SelenideElement imageFileButton = $("#offer-imagefile");
+    private final SelenideElement favImageFileButton = $("#offer-favimagefile");
 
-    private SelenideElement categoryDropdown = $x("//span[@id='select2-offer-categoryid-container']");
-    private SelenideElement categoryFirst = $x("//ul[@id='select2-offer-categoryid-results']/li[1]");
+    // Элементы ввода данных
+    private final SelenideElement name = $("#offer-name");
+    private final SelenideElement slug = $("#offer-slug");
+    private final SelenideElement licence = $("#offer-licensenumber");
+    private final SelenideElement erid = $("#offer-erid");
+    private final SelenideElement hold = $("#offer-holdseconds");
+    private final SelenideElement minAge = $("#offer-age_min");
+    private final SelenideElement maxAge = $("#offer-age_max");
+    private final SelenideElement offerLink = $("#offer-link");
+    private final SelenideElement offerProductName = $("#offer-productname");
 
-    private SelenideElement countryDropdown = $x("//span[@id='select2-offer-country-container']");
-    private SelenideElement countryFirst = $x("//ul[@id='select2-offer-country-results']/li[1]");
+    // Выпадающие списки и их элементы
+    private final SelenideElement statusDropdown = $x("//span[text()='Выберите статус...']");
+    private final SelenideElement activeStatus = $x("//ul/li[text()='Активен']");
+    private final SelenideElement privateStatus = $x("//ul/li[text()='Приватный']");
+    private final SelenideElement noActiveStatus = $x("//ul/li[text()='Неактивен']");
+    private final SelenideElement deleteStatus = $x("//ul/li[text()='Удален']");
+    private final SelenideElement advertiserDropdown = $x("//span[@id='select2-offer-advertiserid-container']");
+    private final SelenideElement advertiserTest = $x("//ul/li[text()='181 TestAdvertis']");
+    private final SelenideElement advertiserFirst = $x("//ul[@id='select2-offer-advertiserid-results']/li[1]");
+    private final SelenideElement currencyDropdown = $x("//span[@id='select2-offer-currency-container']");
+    private final SelenideElement currencyFirst = $x("//ul[@id='select2-offer-currency-results']/li[1]");
+    private final SelenideElement categoryDropdown = $x("//span[@id='select2-offer-categoryid-container']");
+    private final SelenideElement categoryFirst = $x("//ul[@id='select2-offer-categoryid-results']/li[1]");
+    private final SelenideElement countryDropdown = $x("//span[@id='select2-offer-country-container']");
+    private final SelenideElement countryFirst = $x("//ul[@id='select2-offer-country-results']/li[1]");
+    private final SelenideElement trafficBackDropdown = $x("//span[@id='select2-offer-trafficbackid-container']");
+    private final SelenideElement trafficBackFirst = $x("//ul[@id='select2-offer-trafficbackid-results']/li[1]");
 
-    private SelenideElement trafficBackDropdown = $x("//span[@id='select2-offer-trafficbackid-container']");
-    private SelenideElement trafficBackFirst = $x("//ul[@id='select2-offer-trafficbackid-results']/li[1]");
-    public void sendNameActive() {
-        String randomName = "Active_" + faker.name().firstName().toLowerCase();
+    // Методы для работы с полями
+    public void sendName(String prefix) {
+        String randomName = prefix + "_" + faker.name().firstName().toLowerCase();
         name.setValue(randomName);
-    }
-
-    public void sendNamePrivate() {
-        String randomName = "Private_" + faker.name().firstName().toLowerCase();
-        name.setValue(randomName);
-    }
-
-    public void sendNameNoActive() {
-        String randomName = "NoActive_" + faker.name().firstName().toLowerCase();
-        name.setValue(randomName);
-    }
-
-    public void sendNameDelete() {
-        String randomName = "Delete_" + faker.name().firstName().toLowerCase();
-        name.setValue(randomName);
-    }
-
-    public void setStatusActive() {
-        statusDropdown.click();
-        activeStatus.click();
-    }
-
-    public void setStatusPrivate() {
-        statusDropdown.click();
-        privateStatus.click();
-    }
-
-    public void setStatusNoActive() {
-        statusDropdown.click();
-        noActiveStatus.click();
-    }
-
-    public void setStatusDelete() {
-        statusDropdown.click();
-        deleteStatus.click();
     }
 
     public void sendSlug() {
@@ -92,87 +62,63 @@ public class OfferCreationPage {
         offerProductName.setValue(randomOfferProductName);
     }
 
-    public void setAdvertiserTest() {
-        advertiserDropdown.click();
-        advertiserTest.click();
-    }
-
-    public void setAdvertiserFirst() {
-        advertiserDropdown.click();
-        advertiserFirst.click();
-    }
-
-    public void setCurrencyFirst() {
-        currencyDropdown.click();
-        currencyFirst.click();
-    }
-
-    public void setCategoryFirst() {
-       categoryDropdown.click();
-       categoryFirst.click();
-    }
-
-    public void setCountryFirst() {
-        countryDropdown.click();
-        countryFirst.click();
-    }
-
-    public void sendlicence() {
+    public void sendFixedData() {
         licence.setValue("111111");
-    }
-    public void sendErid() {
         erid.setValue("111111");
-    }
-    public void sendMinAge() {
         minAge.setValue("18");
-    }
-
-    public void sendMaxAge() {
         maxAge.setValue("100");
-    }
-
-    public void sendHold() {
         hold.setValue("0");
-    }
-
-    public void sendLink() {
         offerLink.setValue("https://google.by/?clickId={clickId}&wmid={webmasterid}");
     }
 
-    public void setTrafficBackFirst() {
-        trafficBackDropdown.click();
-        trafficBackFirst.click();
+    public void setStatus(String statusName) {
+        statusDropdown.click();
+        switch (statusName) {
+            case "Активен":
+                activeStatus.click();
+                break;
+            case "Приватный":
+                privateStatus.click();
+                break;
+            case "Неактивен":
+                noActiveStatus.click();
+                break;
+            case "Удален":
+                deleteStatus.click();
+                break;
+        }
     }
+
+    // Методы для работы с выпадающими списками
+    public void selectFirstOption(SelenideElement dropdown, SelenideElement firstOption) {
+        dropdown.click();
+        firstOption.click();
+    }
+
+    // Методы для загрузки изображений
     public void uploadImage() {
-        imageFileButton.uploadFile(new File(pathImage));
+        imageFileButton.uploadFile(new File(PATH_IMAGE));
     }
 
     public void uploadFavImage() {
-        favImageFileButton.uploadFile(new File(pathImage));
+        favImageFileButton.uploadFile(new File(PATH_IMAGE));
     }
 
-    public void buttonSaveClick() {
-        buttonSave.click();
-        sleep(1000);// костыль пока
-    }
+    // Общий метод создания предложения
     public void generateOffer() {
-        sendNameActive();
-        setStatusActive();
+        sendName("Active");
+        setStatus("Активен");
         sendSlug();
         sendOfferProductName();
-        setAdvertiserFirst();
-        setCurrencyFirst();
-        setCategoryFirst();
-        setCountryFirst();
-        sendlicence();
-        sendErid();
-        sendMinAge();
-        sendMaxAge();
-        sendLink();
-        sendHold();
+        selectFirstOption(advertiserDropdown, advertiserFirst);
+        selectFirstOption(currencyDropdown, currencyFirst);
+        selectFirstOption(categoryDropdown, categoryFirst);
+        selectFirstOption(countryDropdown, countryFirst);
+        sendFixedData();
         uploadImage();
         uploadFavImage();
-        setTrafficBackFirst();
-        buttonSaveClick();
+        selectFirstOption(trafficBackDropdown, trafficBackFirst);
+        buttonSave.click();
+        sleep(1000);
     }
 }
