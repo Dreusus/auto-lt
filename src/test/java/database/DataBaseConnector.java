1e94649a6cbb9d0879956fcaf99723ea48608c26
dreusus <pitakxdd@gmail.com>
2023-10-28 14:35:03 +0300

    Connection is established, getLatstCreatedWebmaster

--- src/test/java/database/DataBaseConnector.java
+++ src/test/java/database/DataBaseConnector.java
@@ -2,26 +2,20 @@ package database;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.List;
+import java.sql.*;
 import java.util.Properties;
 import java.io.InputStream;
 
 public class DataBaseConnector {
 
-    private static String dbUrl;
-    private static String dbUser;
-    private static String dbPassword;
+    private static final String dbUrl;
+    private static final String dbUser;
+    private static final String dbPassword;
 
     static {
-        try {
-
-            Properties properties = new Properties();
-            InputStream inputStream = new FileInputStream(new File("src/test/java/config/database.properties"));
+        //Загрузка констант из database.properties
+        Properties properties = new Properties();
+        try (InputStream inputStream = new FileInputStream(new File("src/test/java/config/database.properties"))) {
             properties.load(inputStream);
 
             dbUrl = properties.getProperty("db.url");
@@ -31,35 +25,47 @@ public class DataBaseConnector {
             // Загрузка JDBC драйвера
             Class.forName("org.postgresql.Driver");
         } catch (Exception e) {
-            e.printStackTrace();
-            throw new RuntimeException("Failed to initialize database connection parameters");
+            throw new RuntimeException("Failed to initialize database connection parameters", e);
         }
     }
-    public static Connection getConnection() throws Exception {
+
+    //Установление соединения с БД
+    private static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(dbUrl, dbUser, dbPassword);
     }
 
-    public static List<String> getTables() {
-        List<String> tables = new ArrayList<>();
+    public static void printLastCreatedWebmaster() throws SQLException {
+        String query = "SELECT * FROM webmaster ORDER BY \"createDate\" DESC LIMIT 1";
+
         try (Connection connection = getConnection();
              Statement statement = connection.createStatement();
-             ResultSet resultSet = statement.executeQuery("SELECT tablename FROM pg_tables WHERE schemaname = 'public';")) {
+             ResultSet resultSet = statement.executeQuery(query)) {
 
-            while (resultSet.next()) {
-                tables.add(resultSet.getString("tablename"));
+            // Получаем метаданные о ResultSet
+            ResultSetMetaData metaData = resultSet.getMetaData();
+            int columnCount = metaData.getColumnCount();
+
+            if (resultSet.next()) {
+                for (int i = 1; i <= columnCount; i++) {
+                    String columnName = metaData.getColumnName(i);
+                    Object columnValue = resultSet.getObject(i);
+                    // Выводим имя столбца и значение
+                    System.out.println(columnName + ": " + columnValue);
+                }
+            } else {
+                System.out.println("Вебмастер не найден.");
             }
 
-        } catch (Exception e) {
-            e.printStackTrace();
         }
-        return tables;
     }
+
+
 
     public static void main(String[] args) {
-        List<String> tables = getTables();
-        for (String table : tables) {
-            System.out.println(table);
+        try {
+            printLastCreatedWebmaster();
+        } catch (SQLException e) {
+            System.err.println("Произошла ошибка при работе с базой данных: " + e.getMessage());
         }
     }
-
 }
